59f05a2f141b857b4f30c98916b7fae572e2ccfd
UshaDasetty <ushadasetty@gmail.com>
2023-07-07 12:29:25 +0530

    Done with User Authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,10 +4,11 @@ const ErrorResponse = require('../utils/errorResponse');
 
 
 exports.protect = async (req, res, next) => {
-    let token;
+    let token;   
 
     if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        token = req.headers.authorization.split(' ')[1];
+     // This token will split like this from the array and select the second part -> Bearer 42trgf654688hgfgdhj67hhf
+        token = req.headers.authorization.split(' ')[1];  
     }
 
     if(!token) {
@@ -15,14 +16,21 @@ exports.protect = async (req, res, next) => {
     }
 
     try {
+        // If token found it will just decode the token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+
+        // It will find the user with decoded id
         const user =await User.findById(decoded.id);
 
+
+        // If user is not found with that decoded id it will not allow user to access the website
         if(!user) {
             return next(new ErrorResponse("No user found with this id",404));
         }
 
+        
+        // if user found with that decoded id, it will allow to access the website
         req.user = user;
 
         next();
